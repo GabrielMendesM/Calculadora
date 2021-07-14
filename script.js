9251c4938ca04c1ce9f637158ce0a5f76a4bad3f
Gabriel Mendes <81168814+GabrielMendesM@users.noreply.github.com>
2021-07-14 13:46:10 -0300

    refazendo

--- script.js
+++ script.js
@@ -1,3 +1,95 @@
+var res;
+var resAnterior;
+
+function atualizarTela(operacao) {
+    var resTxt = document.getElementById("resultado");
+    var txtAux;
+
+    switch (operacao) {
+        case "somar":
+            txtAux = `${resAnterior} + ${res}`;
+            break;
+
+        case "subtrair":
+            txtAux = `${resAnterior} - ${res}`;
+            break;
+
+        case "multiplicar":
+            txtAux = `${resAnterior} &#215 ${res}`;
+            break;
+
+        case "dividir":
+            txtAux = `${resAnterior} &#247 ${res}`;
+            break;
+            
+        case "limpar":
+            if (resAnterior) {
+                txtAux = String(resAnterior);
+            } else {
+                txtAux = 0;
+            }
+            break;
+
+        case "limparTudo":
+            txtAux = 0;
+            break;
+
+        default:
+            txtAux = String(res);
+            break;
+    }
+
+    resTxt.innerHTML = `<p>${txtAux}</p>`
+}
+
+var numero = (n) => {
+    if (res == 0 && n == 0) {
+        res = 0;
+    }
+    if (res) {
+        res += String(n);
+    }
+    else {
+        res = String(n);
+    }
+    atualizarTela(null);
+}
+
+function limpar() {
+    res = 0;
+    atualizarTela("limpar");
+}
+
+function limparTudo() {
+    resAnterior = 0;
+    res = 0;
+    atualizarTela("limparTudo");
+}
+
+function somar() {
+    if (resAnterior) {
+
+    }
+    atualizarTela("somar");
+}
+
+function subtrair() {
+
+}
+
+function multiplicar() {
+
+}
+
+function dividir() {
+
+}
+
+function calcular() {
+
+}
+
+/*
 var res = null;
 
 var inputUsuário = () => {
@@ -57,3 +149,4 @@ function dividir() {
         alert("Digite um número válido.")
     }
 }
+*/
