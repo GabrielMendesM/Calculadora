ced11866c02abffe496c7eac9abe11f488509c2d
Gabriel Mendes <81168814+GabrielMendesM@users.noreply.github.com>
2021-07-14 17:35:15 -0300

    deu erro

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var res;
 var resAnterior;
+var resAnteriorTxt;
 
 function atualizarTela(operacao) {
     var resTxt = document.getElementById("resultado");
@@ -7,19 +8,23 @@ function atualizarTela(operacao) {
 
     switch (operacao) {
         case "somar":
-            txtAux = `${resAnterior} + ${res}`;
+            resAnteriorTxt = `${resAnterior} + `;
+            txtAux = `${resAnteriorTxt}`;
             break;
 
         case "subtrair":
-            txtAux = `${resAnterior} - ${res}`;
+            resAnteriorTxt = `${resAnterior} - `;
+            txtAux = `${resAnteriorTxt}`;
             break;
 
         case "multiplicar":
-            txtAux = `${resAnterior} &#215 ${res}`;
+            resAnteriorTxt = `${resAnterior} &#215 `;
+            txtAux = `${resAnteriorTxt}`;
             break;
 
         case "dividir":
-            txtAux = `${resAnterior} &#247 ${res}`;
+            resAnteriorTxt = `${resAnterior} &#247 `;
+            txtAux = `${resAnteriorTxt}`;
             break;
             
         case "limpar":
@@ -35,22 +40,36 @@ function atualizarTela(operacao) {
             break;
 
         default:
-            txtAux = String(res);
+            if (resAnterior) {
+                txtAux = `${resAnteriorTxt}${res}`;
+            } else {
+                txtAux = `${res}`;
+            }
             break;
     }
-
-    resTxt.innerHTML = `<p>${txtAux}</p>`
+    resTxt.innerHTML = `<p>${txtAux}</p>`;
 }
 
 var numero = (n) => {
-    if (res == 0 && n == 0) {
-        res = 0;
-    }
-    if (res) {
-        res += String(n);
-    }
-    else {
-        res = String(n);
+    if (n == -1) {
+        if (res !== undefined) {
+            if (String(res).indexOf(",") === -1) {
+                res += ",";
+            }
+        } else {
+            res = "0,";
+        }
+    } else {
+        if (res == 0 && n == 0) {
+            res = 0;
+        } else {
+            if (res) {
+                res == 0 ? res = String(n) : res += String(n);
+            }
+            else {
+                res = String(n);
+            }
+        }
     }
     atualizarTela(null);
 }
@@ -61,34 +80,155 @@ function limpar() {
 }
 
 function limparTudo() {
-    resAnterior = 0;
     res = 0;
+    resAnterior = 0;
+    resAnteriorTxt = undefined;
     atualizarTela("limparTudo");
 }
 
-function somar() {
-    if (resAnterior) {
-        res += resAnterior;
+function operação(op) {
+    if (!res) {
+        res = Number(0);
     } else {
-        resAnterior = res;
+        var v = res.indexOf(",");
+        if (v === -1) {
+            res = String(res).replace(",", ".");
+        }
+        res = Number(res);
     }
-    atualizarTela("somar");
+    if (!resAnterior) {
+        resAnterior = Number(0);
+    } else {
+        resAnterior = Number(resAnterior);
+    }
+    var v = resAnteriorTxt;
+    var soma, subtração, multiplicação, divisão;
+    (function() {
+        v ? soma = v.indexOf("+") : soma = -1,
+        v ? subtração = v.indexOf("-") : subtração = -1,
+        v ? multiplicação = v.indexOf("&#215") : multiplicação = -1,
+        v ? divisão = v.indexOf("&#247") : divisão = -1
+        })();
+    
+    alert(`op: ${op}`);
+    res = Number(res);
+    resAnterior = Number(resAnterior);
+    switch (op) {
+        case "somar":
+            if (soma != -1) {
+                //significa que há uma soma anterior a ser feita
+                resAnterior += res;
+                res = 0;
+
+                /*
+                if (resAnterior) {
+                    resAnterior += res;
+                } else {
+                    resAnterior = res;
+                }
+                res = 0;
+                */
+            } else {
+                
+            }
+            atualizarTela(op);
+            break;
+
+        case "subtrair":
+            if (subtração != -1) {
+                if (resAnterior) {
+                    resAnterior -= res;
+                } else {
+                    resAnterior = res;
+                }
+                res = 0;
+            }
+            atualizarTela(op);
+            break;
+
+        case "multiplicar":
+            if (multiplicação != -1) {
+                if (resAnterior) {
+                    resAnterior *= res;
+                } else {
+                    resAnterior = res;
+                }
+                res = 0;
+            }
+            atualizarTela(op);
+            break;
+
+        case "dividir":
+            if (divisão != -1) {
+                if (resAnterior) {
+                    resAnterior /= res;
+                } else {
+                    resAnterior = res;
+                }
+                res = 0;
+            }
+            atualizarTela(op);
+            break;
+    }
+}
+/*
+function somar() {
+    operacao();
 }
 
 function subtrair() {
+    operacao();
+    
+    if (resAnterior) {
+        resAnterior -= res;
+    } else {
+        resAnterior = res;
+    }
+    res = 0;
 
+    atualizarTela("subtrair");
 }
 
 function multiplicar() {
+    operacao();
 
+    
+    if (resAnterior) {
+        resAnterior *= res;
+    } else {
+        resAnterior = res;
+    }
+    res = 0;
+
+    atualizarTela("multiplicar");
 }
 
 function dividir() {
+    operacao();
 
+    
+    if (resAnterior) {
+        resAnterior /= res;
+    } else {
+        resAnterior = res;
+    }
+    res = 0;
+
+    atualizarTela("dividir");
 }
 
 function calcular() {
+    res = Number(res);
+    resAnterior = Number(resAnterior);
+    
+    if (resAnterior) {
+        resAnterior += res;
+    } else {
+        resAnterior = res;
+    }
+    res = 0;
 
+    atualizarTela("somar");
 }
 
 /*
