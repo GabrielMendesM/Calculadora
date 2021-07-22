d04a293afccdff79f067b99d9c8b6c06982127de
Gabriel Mendes <81168814+GabrielMendesM@users.noreply.github.com>
2021-07-22 20:42:17 -0300

    layout feito

--- vs_react/src/App.js
+++ vs_react/src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import Teste from './componentes/Testes';
+import Botão from './componentes/Botão';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>CALCULADORA</h1>
       </header>
-    </div>
+      <section>
+        <div className="res">
+          <p>0</p>
+        </div>
+        <div className="conta-atual">
+          <p></p></div>
+        <div className="botões">
+          <div className="btns-limpar">
+            <Botão texto="CE"/>
+            <Botão texto="C"/>
+          </div>
+          <div className="btns-num">
+            <Botão texto="1"/>
+            <Botão texto="2"/>
+            <Botão texto="3"/>
+            <Botão texto="4"/>
+            <Botão texto="5"/>
+            <Botão texto="6"/>
+            <Botão texto="7"/>
+            <Botão texto="8"/>
+            <Botão texto="9"/>
+            <Botão texto="0"/>
+            <Botão texto=","/>
+          </div>
+          <div className="btns-oper">
+            <Botão texto="+"/>
+            <Botão texto="-"/>
+            <Botão texto="*"/>
+            <Botão texto="/"/>
+            <Botão texto="="/>
+          </div>
+        </div>
+      </section>
+    </>  
   );
 }
 
